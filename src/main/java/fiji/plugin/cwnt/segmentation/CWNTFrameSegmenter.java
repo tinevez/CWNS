ee06fc7a70002c189c70cb47aa4691b6f73982cd
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-08-27 15:10:00 +0200

    Fix the CWNTFrameSegmenter.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/fiji/plugin/cwnt/segmentation/CWNTFrameSegmenter.java
+++ src/main/java/fiji/plugin/cwnt/segmentation/CWNTFrameSegmenter.java
@@ -1,70 +1,82 @@
 package fiji.plugin.cwnt.segmentation;
 
-import fiji.plugin.trackmate.Settings;
 import fiji.plugin.trackmate.util.TMUtils;
 import ij.ImagePlus;
-import mpicbg.imglib.algorithm.MultiThreadedBenchmarkAlgorithm;
-import mpicbg.imglib.image.Image;
-import mpicbg.imglib.image.display.imagej.ImageJFunctions;
-import mpicbg.imglib.labeling.Labeling;
-import mpicbg.imglib.type.numeric.RGBALegacyType;
 
-public class CWNTFrameSegmenter extends MultiThreadedBenchmarkAlgorithm {
+import java.util.Map;
 
-	private CWNTPanel source;
-	private ImagePlus imp;
+import net.imglib2.algorithm.MultiThreadedBenchmarkAlgorithm;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.labeling.Labeling;
+import net.imglib2.type.numeric.ARGBType;
+
+public class CWNTFrameSegmenter extends MultiThreadedBenchmarkAlgorithm
+{
+
+	private final CWNTPanel source;
+
+	private final ImagePlus imp;
 
 	/*
 	 * CONSTRUCTOR
 	 */
-	
-	public CWNTFrameSegmenter(CWNTPanel panel) {
+
+	public CWNTFrameSegmenter( final CWNTPanel panel )
+	{
 		super();
 		this.source = panel;
 		this.imp = panel.getTargetImagePlus();
 	}
-	
-	
+
 	/*
 	 * METHODS
 	 */
-	
+
 	@Override
-	public boolean checkInput() {
+	public boolean checkInput()
+	{
 		return true;
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings( { "rawtypes", "unchecked" } )
 	@Override
-	public boolean process() {
-		
+	public boolean process()
+	{
+
 		final long start = System.currentTimeMillis();
 		final int frame = imp.getFrame();
-		final Settings settings = new Settings(imp);
-		final Image img = TMUtils.getSingleFrameAsImage(imp, frame-1, settings);
-		final CrownWearingSegmenter cws = new CrownWearingSegmenter();
-		cws.setTarget(img, settings.getCalibration(), source.getSegmenterSettings());
-		
-		if (!cws.process()) {
+
+		final Map< String, Object > settings = source.getSettings();
+
+		final CrownWearingSegmenterFactory factory = new CrownWearingSegmenterFactory();
+		factory.setTarget( TMUtils.rawWraps( imp ), settings );
+
+		final CrownWearingSegmenter cws = factory.getDetector( null, frame );
+		cws.setNumThreads( getNumThreads() );
+
+		if ( !cws.process() )
+		{
 			errorMessage = cws.getErrorMessage();
 			return false;
 		}
-		
+
 		final long end = System.currentTimeMillis();
 		processingTime = end - start;
-		
-		Labeling labels = cws.getLabeling();
-		LabelToRGB converter = new LabelToRGB(labels);
+
+		final Labeling labels = cws.getLabeling();
+		final LabelToRGB converter = new LabelToRGB( labels );
+		converter.setNumThreads( getNumThreads() );
 		converter.process();
-		Image<RGBALegacyType> rgb = converter.getResult();
-		
-		ImagePlus result = ImageJFunctions.copyToImagePlus(rgb);
-		result.setCalibration(imp.getCalibration());
+		final Img< ARGBType > rgb = converter.getResult();
+
+		final ImagePlus result = ImageJFunctions.wrap( rgb, "Labels" );
+		result.setCalibration( imp.getCalibration() );
 		result.show();
-		
-		int tmin = (int) Math.ceil(processingTime / 1e3 / 60); //min 
-		source.labelDurationEstimate.setText("Total duration rough estimate: "+tmin+" min.");
-		
+
+		final int tmin = ( int ) Math.ceil( processingTime / 1e3 / 60 ); // min
+		source.labelDurationEstimate.setText( "Total duration rough estimate: " + tmin + " min." );
+
 		return true;
 	}
 
